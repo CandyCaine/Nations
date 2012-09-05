be80235c1530a1d931956aab926aa4a8c318fa44
CandyCaine <candycainenet@gmail.com>
2012-09-06 00:01:26 +0100

    Began working on Database class

---
+++ src/me/rich/nations/persistence/Database.java
@@ -0,0 +1,37 @@
+package me.rich.nations.persistence;
+
+import lib.PatPeter.SQLibrary.SQLite;
+import me.rich.nations.NationsPlugin;
+
+public class Database {
+
+	private NationsPlugin plugin;
+	private SQLite sqlite;
+
+	public Database(NationsPlugin plugin, String name) {
+		this.plugin = plugin;
+		this.sqlite = new SQLite(this.plugin.getLogger(), this.plugin.getDescription().getFullName(), name, this.plugin.getDataFolder().getAbsolutePath());
+
+		try {
+			this.sqlite.open();
+		} catch (Exception e) {
+			this.plugin.getLogger().info(e.getMessage());
+			this.plugin.getPluginLoader().disablePlugin(this.plugin);
+		}
+		
+		this.checkTable();
+	}
+	
+	private void checkTable() {
+		if (this.sqlite.checkTable("nation")) {
+			return;
+		} else {
+			String query = "CREATE TABLE nation (id INT PRIMARY KEY, nationname VARCHAR(50), description VARCHAR(150));";
+			this.sqlite.query(query);
+		}
+	}
+
+	public void close() {
+		this.sqlite.close();
+	}
+}
